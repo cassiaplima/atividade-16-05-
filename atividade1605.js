b7b31326f7ed693d6f2d2b988ae7a921f94d0a67
Cássia ☆ <102737875+cassiaplima@users.noreply.github.com>
2022-05-16 10:40:17 -0300

    Add files via upload

---
+++ atividade1605.js
@@ -0,0 +1,30 @@
+//atividade - Cassia Pereira Lima - DATA 16/05
+
+// Atividade-01 
+/*let a = 1
+
+while( a <= 100){
+    console.log(a)
+    a = a + 1
+}*/
+
+//atividade-02
+/*let b = 50 
+while(b <= 100){
+    console.log(b)
+    b = b + 1
+}*/
+
+//atividade-03
+/*let c = 10
+while(c >= 0){
+    console.log(c)
+    c = c - 1
+}*/
+
+//atividade-04
+/*let c = 0
+while(c <= 20){
+    console.log(c)
+    c = c + 2
+}*/
